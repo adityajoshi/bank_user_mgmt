ebc0ffb5a66c760f73fbd62383c2d1a1aa29e77a
adityajoshi <aditya.joshi1@outlook.com>
2021-01-12 09:59:59 +0530

    Added delete feature

--- src/main/java/com/spring/rests/controller/UserController.java
+++ src/main/java/com/spring/rests/controller/UserController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +36,16 @@ public class UserController {
 	public List<User> findAll() {
 		Optional<List<User>> data = Optional.ofNullable(userMgmtRepo.findAll());
 		return data.get();
+	}
+
+	@GetMapping(value = "/delete/{id}")
+	public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
+		try {
+			userMgmtRepo.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
